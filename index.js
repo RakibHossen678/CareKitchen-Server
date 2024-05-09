347969f3c3a8bd10c76f52288b960ccc5bf7ba2e
RakibHossen678 <rh491464@gmail.com>
2024-05-09 23:32:27 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.port || 5000;
 
-app.get('/', (req, res) => {
-  res.send('CareKitchen sever is running')
-})
+//Middlewares
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrdje6l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    
+    // Send a ping to confirm a successful connection
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("CareKitchen sever is running");
+});
 
 app.listen(port, () => {
-  console.log(`CareKitchen sever is listening on port ${port}`)
-})
+  console.log(`CareKitchen sever is listening on port ${port}`);
+});
